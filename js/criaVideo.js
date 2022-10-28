2b4812c0abc5785867d0e07143c7451d74153b8d
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-10-28 14:47:07 -0300

    criando logica de pagina de envio

---
+++ js/criaVideo.js
@@ -0,0 +1,21 @@
+
+
+const formulario = document.querySelector ('[data-formulario]')
+
+
+
+function criaVideo (evento) {
+    evento.preventDefault ()
+
+    
+const imagem = document.querySelector ('[data-imagem]')
+
+const url = document.querySelector ('[data-url]')
+
+const titulo = document.querySelector ('[data-titulo]')
+
+const descricao = Math.floor (Math.random()*10).toString ()
+}
+
+
+formulario.addEventListener ("submit", evento => criaVideo (evento))
